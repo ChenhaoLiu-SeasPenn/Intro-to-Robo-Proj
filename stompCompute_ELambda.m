437d2ef10827803232574163b94f80c6570e0771
ChenhaoLiu-SeasPenn <33504592+ChenhaoLiu-SeasPenn@users.noreply.github.com>
2017-12-01 16:37:44 -0500

    Update a function

--- stompCompute_ELambda.m
+++ stompCompute_ELambda.m
@@ -1,5 +1,11 @@
 function E = stompCompute_ELambda(pathCost)
 %Compute the exp(-1/lambda * S) form for Prob computation
 
+h = 10;
+
+maxS = max(pathCost, [], 1);
+minS = min(pathCost, [], 1);
+
+E = exp(-h * (pathCost - minS) ./ (maxS - minS));
 
 end
