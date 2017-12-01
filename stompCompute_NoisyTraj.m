baec0015c4cebe8c47e9f824fa010074131a040e
Zhiheng Zhang <zzh112119@gmail.com>
2017-12-01 17:58:55 -0500

    updated

--- stompCompute_NoisyTraj.m
+++ stompCompute_NoisyTraj.m
@@ -1,22 +1,22 @@
-function [Traj_N,ek]=stompCompute_NoisyTraj(k,Start,End,R)
+function [Traj_N,ek]=stompCompute_NoisyTraj(n_Samples,Start,End,R)
 %THis function computes the noisy trajectory 
 
 Num_k=10;
 Traj=End-Start;
 Step=[Start];
-mu=zeros(1,k);
+mu=zeros(1,n_Samples);
 sigma=R;
 Traj_N=cell(1,Num_k);
 
-for i = 1 : k
-    gap = Traj/k*(i-1);
+for i = 1 : n_Samples
+    gap = Traj/n_Samples*(i-1);
     Step=[Step; Start+gap];
 end
 
 ek = mvnrnd(mu,sigma,Num_k);
 
 for j = 1 : Num_k
-    Traj_N(j)=Step+ek(j,:);
+    Traj_N{j}=Step+ek(j,:);
 end
 
 end
