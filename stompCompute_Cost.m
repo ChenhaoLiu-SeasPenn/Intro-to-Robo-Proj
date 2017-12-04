330aa2310dc946cf902e090bcd7a12092f5835bd
ChenhaoLiu-SeasPenn <33504592+ChenhaoLiu-SeasPenn@users.noreply.github.com>
2017-12-04 15:50:05 -0500

    ComputeCost, update planner

--- stompCompute_Cost.m
+++ stompCompute_Cost.m
@@ -1,7 +1,10 @@
-function Cost = stompCompute_Cost(ntheta, env)
+function Cost = stompCompute_Cost(ntheta, obst, hole)
 %Compute the cost of all points on one path
+Cost = 0;
 
-
+[X, ~] = updateQ(ntheta);
+[robotF, robotR] = stompRobot_Formation(X);
+Cost = Cost + stompCollision_Cost(robotF, robotR, obst, hole);
 
 end
 
