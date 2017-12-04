57bb16ce6d766442ebbc7b3aff55e44796d2f8e9
Zhiheng Zhang <zzh112119@gmail.com>
2017-12-04 16:04:18 -0500

    updated Robot form

--- stompRobot_Formation.m
+++ stompRobot_Formation.m
@@ -9,9 +9,6 @@ a3 = 25;
 a4 = 35;
 
 %circle radius for different parts
-robots{1,2} = 20;
-robots{2,2} = 20;
-robots{3,2} = 20;
 
 %sample numbers for different areas
 n_p1=40;
@@ -23,21 +20,24 @@ y1 = X_mat(2, 2); y2 = X_mat(3, 2);
 z1 = X_mat(2, 3); z2 = X_mat(3, 3);
 P1 = [(linspace(x1,x2,n_p1))',(linspace(y1,y2,n_p1))',(linspace(z1,z2,n_p1))'];
 robots{1,1} = P1;
-
+robots{1,2} = ones(length(P1),1);
+robots{1,2} = 20 * robots{1,2};
 
 x1 = X_mat(3, 1); x2 = X_mat(4, 1);
 y1 = X_mat(3, 2); y2 = X_mat(4, 2);
 z1 = X_mat(3, 3); z2 = X_mat(4, 3);
 P2 = [(linspace(x1,x2,n_p2))',(linspace(y1,y2,n_p2))',(linspace(z1,z2,n_p2))'];
 robots{2,1} = P2; 
-
+robots{2,2} = ones(length(P2),1);
+robots{2,2} = 20 * robots{2,2};
 
 x1 = X_mat(4, 1); x2 = X_mat(6, 1);
 y1 = X_mat(4, 2); y2 = X_mat(6, 2);
 z1 = X_mat(4, 3); z2 = X_mat(6, 3);
 P3 = [(linspace(x1,x2,n_p3))',(linspace(y1,y2,n_p3))',(linspace(z1,z2,n_p3))'];
 robots{3,1} = P3;
-
+robots{3,2} = ones(length(P3),1);
+robots{3,2} = 20 * robots{3,2};
 
 robot = [robots{1,1}; robots{2,1}; robots{3,1}];
 radi = [robots{1,2}; robots{2,2}; robots{3,2}];
